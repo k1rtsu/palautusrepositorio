ce28dfc816d06c0bcaa255db7af898b81c68f93a
Kirill Saukkonen <kirillsaukkonen@gmail.com>
2024-11-11 17:55:36 +0200

    Viikko 2 valmiiksi

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,5 +1,10 @@
 import requests
 from player import Player
+from rich import print
+from rich.prompt import Prompt
+from rich.table import Table
+
+
 
 class PlayerReader:
     def __init__(self, url):
@@ -40,17 +45,36 @@ class PlayerStats:
     
 
 def main():
+    while True:
+        season = Prompt.ask("Select season [purple][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/][/purple]")
+        nationality = Prompt.ask("Select nationality [purple][AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR][/purple]")
 
-    url = 'https://studies.cs.helsinki.fi/nhlstats/2023-24/players'
 
-    reader = PlayerReader(url)
-    stats = PlayerStats(reader)
+        url = 'https://studies.cs.helsinki.fi/nhlstats/2023-24/players'
+        reader = PlayerReader(url)
+        stats = PlayerStats(reader)
 
-    result = stats.top_scorers_by_nationality('FIN')
+        result = stats.top_scorers_by_nationality(nationality)
 
-    print("SUOMI")
-    for p in result:
-        print(p)
+        table = Table(title=f"Top scorers of {nationality} in {season}")
+        table.add_column("name", style="deep_sky_blue1")
+        table.add_column("team", style="magenta")
+        table.add_column("goals", style="green3")
+        table.add_column("assists", style="green3")
+        table.add_column("points", style="green3")
+
+        for player in result:
+            table.add_row(
+                player.name,
+                player.team,
+                str(player.goals),
+                str(player.assists),
+                str(player.goals + player.assists),
+            )
+
+        print(table)
+
+
 
 
 
