5e212cc8853add013a66f462d3efab299ac736b6
Kirill Saukkonen <kirillsaukkonen@gmail.com>
2024-11-22 15:16:36 +0200

    tehtavat 1 ja 1 viikko 4

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,18 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa_kortille_possumma(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(5)
+
+    def test_lataa_kortille_negsumma(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -3)
+
+        maksukortti_mock.lataa.assert_not_called()
+
