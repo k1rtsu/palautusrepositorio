fe8ee9239f6542aa9ff989cccaf277fbeec4ca30
Kirill Saukkonen <kirillsaukkonen@gmail.com>
2024-12-02 19:30:24 +0200

    Tehtava 3

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,47 @@
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.p1 = player1_name
+        self.p2 = player2_name
+        self.p1_score = 0
+        self.p2_score = 0
+
+    def score_types(self, game:int):
+        types = ["Love","Fifteen","Thirty","Forty"]
+        return types[game]
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.p1:
+            self.p1_score += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.p2_score += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        if self.p1_score == self.p2_score:
+            return self.tie_score()
+        elif self.p1_score >= 4 or self.p2_score >=4:
+            return self.lastpoint_score()
+        
+        return self.normal_score()
+    
+    def tie_score(self):
+        if self.p1_score < 3:
+            return f'{self.score_types(self.p1_score)}-All'
+        
+        return f'Deuce'
+    
+    def lastpoint_score(self):
+        dif = self.p1_score - self.p2_score
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+        if dif == 1:
+            return 'Advantage player1'
+        if dif  == -1:
+            return 'Adventage player2'
+        if dif >= 2:
+            return 'Win for player1'
+        
+        return 'Win for player2'
+    
+    def normal_score(self):
+        p1 = self.score_types(self.p1_score)
+        p2 = self.score_types(self.p2_score)
+        return f'{p1}-{p2}'
