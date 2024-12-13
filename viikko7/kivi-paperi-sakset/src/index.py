4e99c39c88169e611c1bd5ef988198146c32b4c8
Kirill Saukkonen <kirillsaukkonen@gmail.com>
2024-12-13 15:46:35 +0200

    Tehtavat 3ja4

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,6 +1,4 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from kps_parempi_tekoaly import KPSParempiTekoaly
+from luo_peli import luo_peli 
 
 
 def main():
@@ -12,31 +10,13 @@ def main():
               "\nMuilla valinnoilla lopetetaan"
               )
 
-        vastaus = input()
+        tyyppi = input()
 
-        if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
+        peli = luo_peli(tyyppi)
+        print("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
 
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
-        else:
-            break
+        peli.pelaa()
+        
 
 
 if __name__ == "__main__":
--- viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
@@ -1,29 +1,13 @@
-from tuomari import Tuomari
 from tekoaly_parannettu import TekoalyParannettu
+from kivipaperisakset import KiviPaperiSakset
+
+class KPSParempiTekoaly(KiviPaperiSakset):
+    def __init__(self):
+        self.tekoaly = TekoalyParannettu(10)
 
 
-class KPSParempiTekoaly:
-    def pelaa(self):
-        tuomari = Tuomari()
-        tekoaly = TekoalyParannettu(10)
+    def _toisen_siirto(self, ensimmaisen_siirto = None):
+        tokan_siirto = self.tekoaly.anna_siirto()
+        self.tekoaly.aseta_siirto(ensimmaisen_siirto)
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = tekoaly.anna_siirto()
-
-        print(f"Tietokone valitsi: {tokan_siirto}")
-
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = tekoaly.anna_siirto()
-
-            print(f"Tietokone valitsi: {tokan_siirto}")
-            tekoaly.aseta_siirto(ekan_siirto)
-
-        print("Kiitos!")
-        print(tuomari)
-
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+        return tokan_siirto
